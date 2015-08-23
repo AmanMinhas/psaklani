d095067a27d8c855acd265aed050d353cdb3bd24
Amandeep Minhas <aman@Amandeeps-MacBook-Pro.local>
2015-08-22 20:40:09 -0700

    constructing homepage

---
+++ client/js/helpers/carousel_homepage_happy_clients.js
@@ -0,0 +1,44 @@
+
+Template.carousel_homepage_happy_clients.helpers({
+	happyClientMessages : function() {
+		
+		carouselData = [
+			{
+				"message" 	: "\"Priya has many years of healing experience, and an impressive array of<br>complementary healing skills, that she synthesizes according to each person's needs.<br>What touches me the most about Priya and what I value most about her, is the deep and<br>genuine compassion she demonstrates for her clients. She nurtures and supports you<br>from a very open heart. I find it healing to be in her presence.\"",
+				"by"		: "Emily Ratakovisky,<br>CEO, Emrta",
+				"active"	: "active"
+			},
+			{
+				"message" 	: "\"Priya has many years of healing experience, and an impressive array of complementary healing<br>skills, that she synthesizes according to each person's needs. What touches me the most about Priya and<br>what I value most about her, is the deep and genuine compassion she demonstrates for her clients.<br>She nurtures and supports you from a very open heart. I find it healing to be in her presence.\"",
+				"by"		: "Emily Ratakovisky,<br>CEO, Emrta",
+				"active"	: ""
+			},
+			{
+				"message" 	: '"Priya has many years of healing experience, and an impressive array of complementary healing<br>skills, that she synthesizes according to each person\'s needs. What touches me the most about Priya and<br>what I value most about her, is the deep and genuine compassion she demonstrates for her clients.<br>She nurtures and supports you from a very open heart. I find it healing to be in her presence."',
+				"by"		: "Emily Ratakovisky,<br>CEO, Emrta",
+				"active"	: ""
+			},
+			{
+				"message" 	: '"Priya has many years of healing experience, and an impressive array of complementary healing<br>skills, that she synthesizes according to each person\'s needs. What touches me the most about Priya and<br>what I value most about her, is the deep and genuine compassion she demonstrates for her clients.<br>She nurtures and supports you from a very open heart. I find it healing to be in her presence."',
+				"by"		: "Emily Ratakovisky,<br>CEO, Emrta",
+				"active"	: ""
+			},
+			{
+				"message" 	: '"Priya has many years of healing experience, and an impressive array of complementary healing<br>skills, that she synthesizes according to each person\'s needs. What touches me the most about Priya and<br>what I value most about her, is the deep and genuine compassion she demonstrates for her clients.<br>She nurtures and supports you from a very open heart. I find it healing to be in her presence."',
+				"by"		: "Emily Ratakovisky,<br>CEO, Emrta",
+				"active"	: ""
+			}
+		];
+
+		return carouselData;	
+	}
+});
+
+
+
+
+
+
+
+
+
---
+++ client/js/script.js
@@ -0,0 +1,38 @@
+
+Template.carousel_homepage_2.helpers({
+	json_homepage_2_carousel_data : function() {
+		
+		carouselData = [
+				{
+					"title" : "Essential Oil Therapy",
+					"image"	: "/img/homepage_images/services/slide1.png",
+					"active": "active"
+				},
+				{
+					"title" : "Essential Oil Therapy",
+					"image"	: "/img/homepage_images/services/slide2.png",
+					"active": ""
+				},
+				{
+					"title" : "Essential Oil Therapy",
+					"image"	: "/img/homepage_images/services/slide3.png",
+					"active": ""
+				},
+				{
+					"title" : "Essential Oil Therapy",
+					"image"	: "/img/homepage_images/services/slide4.png",
+					"active": ""
+				}];
+
+		return carouselData;	
+	}
+});
+
+
+
+
+
+
+
+
+
