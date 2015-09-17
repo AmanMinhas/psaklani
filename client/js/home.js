66b54d6f5a0dafb56935c135a689506a94d76962
Aman <amandeepSinghMinhas@gmail.com>
2015-09-17 16:28:33 -0700

    active class in nav taken care of and menu location on homepage static at bottom if scroll if over the og position

--- client/js/home.js
+++ client/js/home.js
@@ -2,13 +2,12 @@
 
 Template.home.rendered = function() {
 	//To stick the menu to the bottom of the page till it reaches its actual location at the bottom of the image. Remove the class "navbar-fixed-bottom" once the page is scrolled to appropriate position
-	var numScreenHeight = $(window).height();
-	var numHomepageImageHeight = $("#homepage_1").height();
-
 	$(".homepage_1 #my-navbar-nav").addClass("navbar-fixed-bottom");
 
 	$(window).scroll(function(){
-	
+		var numScreenHeight = $(window).height();
+		var numHomepageImageHeight = $("#homepage_1").height();
+
 		var numPageTopPos = $(window).scrollTop();
 		var numPageBottomPos = $(window).scrollTop() + numScreenHeight;
 		
