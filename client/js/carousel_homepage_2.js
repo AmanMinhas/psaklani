a967fe98202516c2c4e0430bc85f181b6426d41d
Amandeep Minhas <aman@Amandeeps-MacBook-Pro.local>
2015-10-04 00:08:23 -0700

    homepage carousel imgs changed, style fixed for tribute to cola page, adding image on contact page - styling pending

--- client/js/carousel_homepage_2.js
+++ client/js/carousel_homepage_2.js
@@ -1,28 +1,45 @@
 Template.carousel_homepage_2.helpers({
 	json_homepage_2_carousel_data : function() {
 		
-		console.log(cServices.find().fetch());
+		// console.log(cServices.find().fetch());
 		carouselData = [
 				{
-					"title" : "Essential Oil Therapy",
-					"image"	: "/img/homepage_images/services-carousel/banner-essential-oil-therapy.jpg",
+					"title" : "Reiki for Pets",
+					"image"	: "/img/homepage_images/services-carousel/banner-reiki-for-animals.jpg",
+					"slug"	: "reiki_for_pets",
 					"active": "active"
 				},
 				{
-					"title" : "Essential Oil Therapy",
-					"image"	: "/img/homepage_images/services/slide2.png",
+					"title" : "Reiki for Humans",
+					"image"	: "/img/homepage_images/services-carousel/banner-reiki-for-animals.jpg",
+					"slug"	: "reiki_for_animals",
+					"active": ""
+				},
+				{
+					"title" : "Hollistic Life Coaching",
+					"image"	: "/img/homepage_images/services-carousel/banner-hollistic-life-coaching.jpg",
+					"slug"	: "hollistic_life_coaching",
 					"active": ""
 				},
 				{
 					"title" : "Essential Oil Therapy",
-					"image"	: "/img/homepage_images/services/slide3.png",
+					"image"	: "/img/homepage_images/services-carousel/banner-essential-oil-therapy.jpg",
+					"slug"	: "essential_oil_therapy",
 					"active": ""
 				},
 				{
-					"title" : "Essential Oil Therapy",
-					"image"	: "/img/homepage_images/services/slide4.png",
+					"title" : "Flower Essence Therapy",
+					"image"	: "/img/homepage_images/services-carousel/banner-flower-essence-therapy.jpg",
+					"slug"	: "flower_essence_therapy",
 					"active": ""
-				}];
+				},
+				{
+					"title" : "Orgone Therapy",
+					"image"	: "/img/homepage_images/services-carousel/banner-orgone-therapy.jpg",
+					"slug"	: "orgone_therapy",
+					"active": ""
+				}
+				];
 
 		return carouselData;	
 	}
