ac933c22a81d07e472d61a476e239a0e672d3537
AmanMinhas <amandeepSinghMinhas@gmail.com>
2015-10-12 22:55:02 -0700

    recent blog post will b a carousel only if blogs.length more than 1. Social Links added

--- client/js/helpers/global/global_helpers.js
+++ client/js/helpers/global/global_helpers.js
@@ -6,7 +6,11 @@ Template.registerHelper('contactInfo',function() {
 	contact = {
 		"email" : "priyasaklani@gmail.com",
 		"mobile" : "+91 9920 313 294",
-		"location" : "Mumbai, India"
+		"location" : "Mumbai, India",
+		"facebook_link" : "https://www.facebook.com/Priya-Saklani-Reiki-Healer-169819829875766/timeline/",
+		"linkedin_link" : "https://in.linkedin.com/pub/dr-priya-saklani-rmt-chlc/2a/368/875",
+		"twitter_link" : "https://twitter.com/BodySoulHealing",
+		"blog_link" : "http://theanimalhealer.blogspot.in/"
 	}
 
 	return contact;	
